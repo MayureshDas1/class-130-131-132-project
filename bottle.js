a58a506b846f9d248be2fa0a38fbdd0be53e5d2c
MayureshDas1 <98962278+MayureshDas1@users.noreply.github.com>
2022-02-03 21:10:11 +0530

    Add files via upload

---
+++ bottle.js
@@ -0,0 +1,33 @@
+img = "";
+statuss = "";
+objects1 = [];
+
+function preload()
+{
+    img = loadImage("bottle2.webp");
+}
+
+function  setup()
+{
+    canvas = createCanvas(600,420);
+    canvas.center();
+    object_detector = ml5.objectDetector("cocossd",modelLoaded)
+}
+
+function modelLoaded()
+{
+    console.log("Model loaded")
+    statuss = true
+    object_detector.detect(img,gotResults)
+}
+
+function gotResults(error, results) 
+{ 
+    if (error) 
+    { 
+        console.log(error);
+     } 
+     console.log(results); 
+     objects1 = results;
+     }
+
